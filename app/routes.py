1fe70f7f0121592cbbffbb24bd63126479835d87
xanderdemidoff <xanderdemidoff@gmail.com>
2019-04-28 20:57:10 +0300

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,8 @@
+from app import app
+
+
+@app.route('/')
+def main_page():
+    """Главный экран. Должен отображать форму ввода данных для поиска
+    в базе Интерпола 'Red label' """
+    return None
